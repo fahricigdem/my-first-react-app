200dc54e83c7d90619ba879b2996ac1b6ffc8c5b
Fahri Cigdem <fahricigdem34@gmail.com>
2021-02-05 11:13:51 +0100

    Create index - ver2.js

---
+++ src src-ver-1/index - ver2.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const element = <h1>Erste Schritt in React index.js, h1 element</h1>;
+ReactDOM.render(element, document.getElementById('root'));
+
+
+
+
+
+
